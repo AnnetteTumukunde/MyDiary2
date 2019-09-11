bf8f4b77ee01e5d8938bee4312f6e974c5376744
AnnetteTumukunde <annettumukunde@akilahinstitute.org>
2019-09-12 00:47:57 +0200

    ft(MyDiary#Chall2): create a get specific entry api endpoint
     - wrote the logic for getting one specified entry
     - created a route to the endpoint
     - tested the response to the request in postman
    
     [ WIP-168346226 ]

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import allroutes from './Server/Routes/allRoutes';
 const app = express();
 app.use(express.json());
 app.use('/entries',allroutes);
+app.use('/entries/entry',allroutes);
 app.use('/', (request, response) => {
     response.send('Just checking the server.');
 });
